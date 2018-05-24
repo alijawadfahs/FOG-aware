ecffcefa1c63f1e57ba00e1f66f88d6d0fd51e63
alijawadfahs <alijawadfahs@gmail.com>
2018-05-24 09:02:18 +0000

    add the logging file to he command.py

--- iptables/command.py
+++ iptables/command.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
-
+import logging
+LOG_FILENAME = 'command.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
 import commands
 
 def OutReturn(com): # testing purposes
-	print "Running " + com
+	logging.info("Running " + com)
 	tup = commands.getstatusoutput(com)
 	return tup[1]
 
 def GetIpRules(ID):
-	print "Running " + "iptables -t nat -L "+ID
+	logging.info("Running " + "iptables -t nat -L "+ID)
 	tup = commands.getstatusoutput("iptables -t nat -L "+ID) 
 	return tup[1]
 def applyIpRule(ID,IP):
 	return
 
 def GetIpLatency(IP):
-	print "Running "+"ping -c 4 " + IP + " | tail -1| awk '{print $4}' | cut -d '/' -f 2"
+	logging.info("Running "+"ping -c 4 " + IP + " | tail -1| awk '{print $4}' | cut -d '/' -f 2")
 	tup = commands.getstatusoutput("ping -c 4 " + IP + " | tail -1| awk '{print $4}' | cut -d '/' -f 2")
 	if tup[1]=='':
 		return 100000
