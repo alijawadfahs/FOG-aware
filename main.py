f3d08f35eed76f23b858dce6c023cc75d6ba4e29
alijawadfahs <alijawadfahs@gmail.com>
2018-07-23 13:33:30 +0000

    defining the new options for the load-balancing

--- main.py
+++ main.py
@@ -16,21 +16,43 @@ def parseCliOptions():
 		parser = argparse.ArgumentParser()
 
 		parser.add_argument( '--command',
-				dest       = 'command',
-				nargs      = '+',
-				type       = str,
-				default    = ['ls'],
-				help       = 'command to the get status output',
+			dest       = 'command',
+			nargs      = '+',
+			type       = str,
+			default    = ['ls'],
+			help       = 'command to the get status output',
 		)
 
 		parser.add_argument( '--cycle',
-				dest       = 'cycle',
-				nargs      = '+',
-				type       = int,
-				default    = [1],
-				help       = 'cycle time in seconds',
+			dest       = 'cycle',
+			nargs      = '+',
+			type       = int,
+			default    = [1],
+			help       = 'cycle time in seconds',
 		)
 
+		parser.add_argument( '--alpha',
+			dest       = 'alpha',
+			nargs      = '+',
+			type       = float,
+			default    = [.8],
+			help       = 'The importance of the location in scheduling',
+		)
+
+		parser.add_argument( '--LoadBalancing',
+			dest       = 'LB',
+			nargs      = '+',
+			type       = str,
+			default    = ['1-best'],
+			help       = 'The function that will decide how probability should be divided over the pods',
+		)
+		parser.add_argument( '--K',
+			dest       = 'K',
+			nargs      = '+',
+			type       = int,
+			default    = [1],
+			help       = 'number of best pods',
+		)
 
 
 		options        = parser.parse_args()
@@ -38,6 +60,9 @@ def parseCliOptions():
 
 
 ##################################### MAIN ###############################################
+def GetOptions(options):
+	return options
+
 def Run(options):
 	BestList   = []
 	svcl       = []
